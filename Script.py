e2ff03daed2f5f6b2cd009d83aac70c1998961db
Apnamovi <99548630+Apnamovi@users.noreply.github.com>
2023-03-01 23:47:15 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,22 +4,8 @@ class script(object):
 ✯ 1 Month = 20rs
 ✯ 3 month = 55rs
 ✯ 6 month = 100rs</b>"""
-
-JEETU_TXT = """<b>Hᴇʟʟᴏ {},
-Bʏ Pʀᴇᴍɪᴜᴍ Pʟᴀɴ Aɴᴅ Wᴀᴛᴄʜ Dɪʀᴇᴄᴛ Fɪʟᴇ Mᴏᴠɪᴇꜱ
-Cʟɪᴄᴋ Mᴇ Tᴏ 👉 <a href=https://cosmofeed.com/vig/63f24db0a03c46001fc9e803>𝐁𝐲 𝐏𝐥𝐚𝐧</a> 👈 Aɴᴅ Eɴɪᴏʏ
-
-✯ 𝟷 Mᴏɴᴛʜ = 𝟸𝟶ʀꜱ
-✯ 𝟹 ᴍᴏɴᴛʜ = 𝟻𝟻ʀꜱ
-✯ 𝟼 ᴍᴏɴᴛʜ = 𝟷𝟶𝟶ʀꜱ
-
-🌹 Cʟɪᴄᴋ Bʏᴇ Lɪɴᴋ 👉 <a href=https://cosmofeed.com/vig/63f24db0a03c46001fc9e803>𝐁𝐲 𝐏𝐥𝐚𝐧</a> 👈
-🌹 Sᴇʟᴇᴄᴛ Pʟᴀɴ
-🌹 Eɴᴛᴇʀ Mᴏʙʟᴇ Nᴜᴍʙᴇʀ
-🌹 Pᴀʏᴍᴇɴᴛ Gᴏᴏɢʟᴇ Pᴀʏ, PʜᴏɴᴇPᴀʏ, Pᴀʏᴛᴍ
-🌹 Yᴏᴜ Gᴇᴛ Jᴏɪɴ Lɪɴᴋ</b>"""
     
-    HELP_TXT = """<b>Hᴇʟʟᴏ {},
+    HELP_TXT = """<b>Hᴇʟʟᴏ {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂.</b>"""
 
 
@@ -355,6 +341,20 @@ Name - {}
 𝖤𝗑𝖺𝗆𝗉𝗅𝖾 :- <code>/country India</code>"""
     UPDATES_TXT = """⍟ Cʜᴀɴɴᴇʟs & Gʀᴏᴜᴘs Mᴏᴅᴜʟᴇ ⍟
 🗣️ Bᴏᴛ Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ.
+
+JEETU_TXT = """<b>Hᴇʟʟᴏ {},
+Bʏ Pʀᴇᴍɪᴜᴍ Pʟᴀɴ Aɴᴅ Wᴀᴛᴄʜ Dɪʀᴇᴄᴛ Fɪʟᴇ Mᴏᴠɪᴇꜱ
+Cʟɪᴄᴋ Mᴇ Tᴏ 👉 <a href=https://cosmofeed.com/vig/63f24db0a03c46001fc9e803>𝐁𝐲 𝐏𝐥𝐚𝐧</a> 👈 Aɴᴅ Eɴɪᴏʏ
+
+✯ 𝟷 Mᴏɴᴛʜ = 𝟸𝟶ʀꜱ
+✯ 𝟹 ᴍᴏɴᴛʜ = 𝟻𝟻ʀꜱ
+✯ 𝟼 ᴍᴏɴᴛʜ = 𝟷𝟶𝟶ʀꜱ
+
+🌹 Cʟɪᴄᴋ Bʏᴇ Lɪɴᴋ 👉 <a href=https://cosmofeed.com/vig/63f24db0a03c46001fc9e803>𝐁𝐲 𝐏𝐥𝐚𝐧</a> 👈
+🌹 Sᴇʟᴇᴄᴛ Pʟᴀɴ
+🌹 Eɴᴛᴇʀ Mᴏʙʟᴇ Nᴜᴍʙᴇʀ
+🌹 Pᴀʏᴍᴇɴᴛ Gᴏᴏɢʟᴇ Pᴀʏ, PʜᴏɴᴇPᴀʏ, Pᴀʏᴛᴍ
+🌹 Yᴏᴜ Gᴇᴛ Jᴏɪɴ Lɪɴᴋ</b>"""
 
 📢 Bᴏᴛ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ."""
     ZOMBIES_TXT = """𝙷𝙴𝙻𝙿 𝚈𝙾𝚄 𝚃𝙾 𝙺𝙸𝙲𝙺 𝚄𝚂𝙴𝚁𝚂
