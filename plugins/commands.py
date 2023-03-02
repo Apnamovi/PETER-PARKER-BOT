9f917ac2de3165f5bb8e6c2a0943a5564df8d6ef
Apnamovi <99548630+Apnamovi@users.noreply.github.com>
2023-03-02 20:05:59 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -42,7 +42,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('Bʏ Sᴜʙꜱᴄʀɪᴘᴛɪᴏɴ', url='https://cosmofeed.com/vig/63f24db0a03c46001fc9e803')
+            InlineKeyboardButton('👉 Bʏ Sᴜʙꜱᴄʀɪᴘᴛɪᴏɴ 👈', url='https://cosmofeed.com/vig/63f24db0a03c46001fc9e803')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         m=await message.reply_sticker("CAACAgUAAxkBAAIRBGPwcjyu8rC9ISXtapMGNc9VxFvOAAInAQACyJRkFOI9YoCRRKaaHgQ") 
@@ -85,7 +85,7 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('Bʏ Sᴜʙꜱᴄʀɪᴘᴛɪᴏɴ', url='https://cosmofeed.com/vig/63f24db0a03c46001fc9e803')
+            InlineKeyboardButton('👉 Bʏ Sᴜʙꜱᴄʀɪᴘᴛɪᴏɴ 👈', url='https://cosmofeed.com/vig/63f24db0a03c46001fc9e803')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
